ea6147549e44856240ea7a47237726736300584e
Ilyukina <anyailyukina@yandex.ru>
2023-08-08 23:35:39 +0800

    Верстка формы

---
+++ src/components/UI/UIInput.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+defineProps<{
+  modelValue: String,
+  label: String,
+}>();
+
+</script>
+
+<template>
+  <div class="input-box">
+    <label class="input-box__label">
+      {{ label}}
+    </label>
+    <input
+      :value="modelValue"
+      class="input-box__input"
+    >
+  </div>
+</template>
+
+<style scoped>
+@import '../../assets/style/u-i-input.css';
+</style>
--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,9 +1,65 @@
 <script setup lang="ts">
 
+import { reactive } from 'vue';
+
+import UIInput from './UI/UIInput.vue';
+
+let dataUser = reactive({
+  name: '',
+  age: '',
+  children: [{
+    name: '',
+    age: '',
+  },
+  {
+    name: '',
+    age: '',
+  }]
+})
+
 </script>
 
 <template>
-  <div>
-    UserForm
-  </div>
+  <form class="user-form">
+    <h2 class="user-form__title">Персональные данные</h2>
+    <u-i-input
+      v-model="dataUser.name"
+      label="Имя"
+      />
+    <u-i-input
+      v-model="dataUser.age"
+      label="Возраст"
+    />
+    <div class="user-form__add-child">
+      <p>Дети (макс.5)</p>
+      <button class="user-form__add-button">+ Добавить ребенка</button>
+    </div>
+    <div>
+      <ul class="child-list">
+        <li
+          v-for="(child, index) in dataUser.children"
+          :key="index"
+          class="child-item"
+          >
+            <u-i-input
+              v-model="child.name"
+              label="Имя"
+            />
+            <u-i-input
+              v-model="child.age"
+              label="Возраст"
+            />
+            <button class="child-item__delete-button">Удалить</button>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <button class="user-form__save-button">Сохранить</button>
+    </div>
+  </form>
 </template>
+
+<style scoped>
+@import '../assets/style/user-form.css';
+
+</style>
